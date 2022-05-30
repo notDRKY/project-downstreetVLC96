a6574cd26ac024f8aba95950539481e4a7096726
notDRKY <93716912+notDRKY@users.noreply.github.com>
2022-05-30 20:45:06 +0200

    v3.1 - 22/05/30

--- Partida.java
+++ Partida.java
@@ -25,8 +25,8 @@ public class Partida {
     private final String[] MAPAS_DISPONIBLES = {"EL CABAÑAL", "EL CARMEN", "LA FONTSANTA", "EL POLÍGONO", "EL METRO", "EL BARRIO CHINO"};
 
     /**
-     * ArrayList dónde se almacenaran los Mosntruos de cada zona (un ArrayList por 
-     * zona).
+     * ArrayList dónde se almacenaran los Mosntruos de cada zona (un ArrayList
+     * por zona).
      */
     private ArrayList<Monstruos> listaMonstruos1 = new ArrayList<>();
 
@@ -47,10 +47,11 @@ public class Partida {
      * Mapa actual.
      */
     private String mapa;
-    
+
     /*
-     * Constructor Partida, no recibirá parámetros, y lanzará los métodos eleccionJugador()
-     * (para asignar Jugador) y mapaAleatorio()(para asignar el mapa).
+     * Constructor Partida, no recibirá parámetros, y lanzará los métodos
+     * eleccionJugador() (para asignar Jugador) y mapaAleatorio()(para asignar
+     * el mapa).
      */
     public Partida() {
         this.jugador = eleccionJugador();
@@ -58,11 +59,10 @@ public class Partida {
     }
 
     /**
-     * lecturaMonstruos leerá un archivo de texto y cargará los datos en los ArrayList
-     * de Monstruos (tantos cómo se tengan).
+     * lecturaMonstruos leerá un archivo de texto y cargará los datos en los
+     * ArrayList de Monstruos (tantos cómo se tengan).
      */
     private void lecturaMonstruos() throws IOException {
-        Scanner s = new Scanner("src/config.txt");
         int saludMaxM;
         double evasionM;
         int danoM;
@@ -74,63 +74,78 @@ public class Partida {
                 BufferedReader br = new BufferedReader(fr)) {
             String linea;
             while ((linea = br.readLine()) != null) {
-                for (int i = 0; i < s.nextInt(); i++) {
-                    saludMaxM = s.nextInt();
-                    evasionM = s.nextDouble();
-                    danoM = s.nextInt();
-                    esJefeM = s.nextBoolean();
-                    criticoM = s.nextDouble();
-                    roboSaludM = s.nextDouble();
+                for (int i = 0; i < Integer.valueOf(linea); i++) {
+                    String[] datos = br.readLine().split(" ");
+                    saludMaxM = Integer.valueOf(datos[0]);
+                    evasionM = Double.valueOf(datos[1]);
+                    danoM = Integer.valueOf(datos[2]);
+                    esJefeM = Boolean.valueOf(datos[3]);
+                    criticoM = Double.valueOf(datos[4]);
+                    roboSaludM = Double.valueOf(datos[5]);
 
-                    listaMonstruos1.add(new Monstruos(saludMaxM, danoM, esJefeM, evasionM, criticoM, roboSaludM));
+                    listaMonstruos1.add(new Monstruos(saludMaxM, danoM, esJefeM,
+                            evasionM, criticoM, roboSaludM));
                 }
-                for (int i = 0; i < s.nextInt(); i++) {
-                    saludMaxM = s.nextInt();
-                    evasionM = s.nextDouble();
-                    danoM = s.nextInt();
-                    esJefeM = s.nextBoolean();
-                    criticoM = s.nextDouble();
-                    roboSaludM = s.nextDouble();
+                linea = br.readLine();
+                for (int i = 0; i < Integer.valueOf(linea); i++) {
+                    String[] datos = br.readLine().split(" ");
+                    saludMaxM = Integer.valueOf(datos[0]);
+                    evasionM = Double.valueOf(datos[1]);
+                    danoM = Integer.valueOf(datos[2]);
+                    esJefeM = Boolean.valueOf(datos[3]);
+                    criticoM = Double.valueOf(datos[4]);
+                    roboSaludM = Double.valueOf(datos[5]);
 
-                    listaMonstruos2.add(new Monstruos(saludMaxM, danoM, esJefeM, evasionM, criticoM, roboSaludM));
+                    listaMonstruos2.add(new Monstruos(saludMaxM, danoM, esJefeM,
+                            evasionM, criticoM, roboSaludM));
                 }
-                for (int i = 0; i < s.nextInt(); i++) {
-                    saludMaxM = s.nextInt();
-                    evasionM = s.nextDouble();
-                    danoM = s.nextInt();
-                    esJefeM = s.nextBoolean();
-                    criticoM = s.nextDouble();
-                    roboSaludM = s.nextDouble();
+                linea = br.readLine();
+                for (int i = 0; i < Integer.valueOf(linea); i++) {
+                    String[] datos = br.readLine().split(" ");
+                    saludMaxM = Integer.valueOf(datos[0]);
+                    evasionM = Double.valueOf(datos[1]);
+                    danoM = Integer.valueOf(datos[2]);
+                    esJefeM = Boolean.valueOf(datos[3]);
+                    criticoM = Double.valueOf(datos[4]);
+                    roboSaludM = Double.valueOf(datos[5]);
 
-                    listaMonstruos3.add(new Monstruos(saludMaxM, danoM, esJefeM, evasionM, criticoM, roboSaludM));
+                    listaMonstruos3.add(new Monstruos(saludMaxM, danoM, esJefeM,
+                            evasionM, criticoM, roboSaludM));
                 }
-                for (int i = 0; i < s.nextInt(); i++) {
-                    saludMaxM = s.nextInt();
-                    evasionM = s.nextDouble();
-                    danoM = s.nextInt();
-                    esJefeM = s.nextBoolean();
-                    criticoM = s.nextDouble();
-                    roboSaludM = s.nextDouble();
+                linea = br.readLine();
+                for (int i = 0; i < Integer.valueOf(linea); i++) {
+                    String[] datos = br.readLine().split(" ");
+                    saludMaxM = Integer.valueOf(datos[0]);
+                    evasionM = Double.valueOf(datos[1]);
+                    danoM = Integer.valueOf(datos[2]);
+                    esJefeM = Boolean.valueOf(datos[3]);
+                    criticoM = Double.valueOf(datos[4]);
+                    roboSaludM = Double.valueOf(datos[5]);
 
-                    listaMonstruos4.add(new Monstruos(saludMaxM, danoM, esJefeM, evasionM, criticoM, roboSaludM));
+                    listaMonstruos4.add(new Monstruos(saludMaxM, danoM, esJefeM,
+                            evasionM, criticoM, roboSaludM));
                 }
-                for (int i = 0; i < s.nextInt(); i++) {
-                    saludMaxM = s.nextInt();
-                    evasionM = s.nextDouble();
-                    danoM = s.nextInt();
-                    esJefeM = s.nextBoolean();
-                    criticoM = s.nextDouble();
-                    roboSaludM = s.nextDouble();
+                linea = br.readLine();
+                for (int i = 0; i < Integer.valueOf(linea); i++) {
+                    String[] datos = br.readLine().split(" ");
+                    saludMaxM = Integer.valueOf(datos[0]);
+                    evasionM = Double.valueOf(datos[1]);
+                    danoM = Integer.valueOf(datos[2]);
+                    esJefeM = Boolean.valueOf(datos[3]);
+                    criticoM = Double.valueOf(datos[4]);
+                    roboSaludM = Double.valueOf(datos[5]);
 
-                    listaMonstruos5.add(new Monstruos(saludMaxM, danoM, esJefeM, evasionM, criticoM, roboSaludM));
+                    listaMonstruos5.add(new Monstruos(saludMaxM, danoM, esJefeM,
+                            evasionM, criticoM, roboSaludM));
                 }
             }
         }
     }
-    
+
     /**
-     * eleccionJugador() lanza un mini menú donde se configurará el Jugador de 
+     * eleccionJugador() lanza un mini menú donde se configurará el Jugador de
      * la Partida.
+     *
      * @return | Devuelve el Jugador ya configurado.
      */
     private Jugador eleccionJugador() {
@@ -138,116 +153,149 @@ public class Partida {
         String nombrePj;
         int tipoPj;
         System.out.println("CREACIÓN PJ");
-        System.out.print("Nombre: ");
+        System.out.print("NOMBRE: ");
         nombrePj = teclado.nextLine();
         do {
-            System.out.println("Clase:\n(0)GUERRERO\n(1)PÍCARO\n(2)MAGO");
+            System.out.println("CLASES:\n[0] -> GUERRERO\n[1] -> PÍCARO\n[2] -> MAGO");
             tipoPj = teclado.nextInt();
         } while (!(tipoPj >= 0 && tipoPj < 3));
 
-        teclado.nextLine();
+        //teclado.nextLine();
 
         Jugador j = new Jugador(nombrePj, tipoPj);
         System.out.println("TÚ PERSONAJE SERÁ: ");
         System.out.println(j.toString());
         return j;
     }
-    
+
     /**
      * seJuegaCartas determina si se lanza el juego de cartas en el método
      * combate.
-     * @return | Devuelve un boolean que determina si se lanza el juego de cartas en el método
-     * combate.
+     *
+     * @return | Devuelve un boolean que determina si se lanza el juego de
+     * cartas en el método combate.
      */
     private boolean seJuegaCartas() {
-        return (0.05 <= Math.round(Math.random() + 1));
+        return 35 > (Math.random() * 100) + 1;
     }
-    
+
     /**
-     * combate() intercambia los ataques de Jugador y Monstruos hasta que la salud
-     * de uno de los dos llegue a 0, también se tiene que llamar al método se juega
-     * cartas para comprobar si se juega o no.
-     * @return | Devuelve un boolean que indica si Jugador ha sido eliminado
-     * o no.
+     * combate() intercambia los ataques de Jugador y Monstruos hasta que la
+     * salud de uno de los dos llegue a 0, también se tiene que llamar al método
+     * se juega cartas para comprobar si se juega o no.
+     *
+     * @return | Devuelve un boolean que indica si Jugador ha sido eliminado o
+     * no.
      */
     private boolean combate(Monstruos m) {
         if (seJuegaCartas()) {
             Scanner teclado = new Scanner(System.in);
             String decision;
-            int c1 = (int)Math.round(Math.random() + 1);
-            int c2 = (int)Math.round(Math.random() + 1);
-            System.out.println("SE JUEGA A LAS CARTAS");
-            System.out.println("La primera carta es: " + c1);
-            System.out.println("La segunda carta será mayor o menor?");
+            int c1 = (int) Math.round(Math.random() * 10) + 1;
+            System.out.println("¡SE JUEGA A LAS CARTAS!");
+            System.out.println("La primera carta es un: " + c1);
+            System.out.print("La segunda carta será mayor o menor?");
+            int c2 = (int) Math.round(Math.random() * 10) + 1;
+
             decision = teclado.nextLine();
             if (decision.equals("mayor") && c2 > c1) {
+                System.out.println("Correcto! La carta era: " + c2);
                 return true;
-            } else return decision.equals("menor") && c2 < c1;
-        } else {
-            while (jugador.getSaludRestante() > 0 && m.getSaludRestante() > 0) {
-                jugador.ataqueJ(m);
-                m.ataqueM(jugador);
+            } else if (decision.equals("menor") && c2 < c1) {
+                System.out.println("Correcto! La carta era: " + c2);
+                return true;
+            } else {
+                System.out.println("Incorrecto! La carta era: " + c2);
+                /*
+                 * Si comentamos este return cuando Jugador falle en el
+                 * juego de cartas, se ejecutará el while de más abajo
+                 * que es justo lo que queremos.
+                 */
+                return false;
             }
-            return jugador.getSaludRestante() >= 0;
         }
+        while (jugador.getSaludRestante() > 0 && m.getSaludRestante() > 0) {
+            jugador.ataqueJ(m);
+            System.out.println(jugador.getNombre() + " ataca! -> [" + jugador.getNombre()
+                + " inflinge " + jugador.getDano() + "!]");
+            
+            m.ataqueM(jugador);
+            System.out.println("!Te ha atacado un monstruo! -> [te han infligido "
+                + m.getDano() + ", te queda " + jugador.getSaludRestante() + "/" + jugador.getSaludMax()
+                + " salud restante]");
+        }
+        return (jugador.getSaludRestante() > 0);
     }
-    
+
     /**
      * jugar() tendrá las "zonas" (arraylist) de la Partida y llamará al método
-     * combate para enfrentar a Jugador contra los Mosntruos de cada zona,
-     * para controlar que jugar sigue en pie, se usa el boolean siguesVivo
-     * que será = llamar a combate(arraylistX.get()). Se deberá lanzar el método
-     * que carga los Monstruos en los arraylist lecturaMonstruos().
+     * combate para enfrentar a Jugador contra los Mosntruos de cada zona, para
+     * controlar que jugar sigue en pie, se usa el boolean siguesVivo que será =
+     * llamar a combate(arraylistX.get()). Se deberá lanzar el método que carga
+     * los Monstruos en los arraylist lecturaMonstruos().
      */
     private void jugar() {
+        Scanner teclado = new Scanner(System.in);
         boolean siguesVivo = true;
-        while (siguesVivo) {
-            try {
-                lecturaMonstruos();
-            } catch (IOException ex) {
-                System.out.println("¡ERROR EN LA LECTURA DE MONSTRUOS!");
-            }
-            for (int i = 0; i < listaMonstruos1.size(); i++) {
-                this.mapa = mapaAleatorio();
-                System.out.println("ESTAS EN: " + this.mapa);
-                siguesVivo = combate(listaMonstruos1.get(i));
-            }
-            for (int i = 0; i < listaMonstruos2.size(); i++) {
-                this.mapa = mapaAleatorio();
-                System.out.println("ESTAS EN: " + this.mapa);
-                siguesVivo = combate(listaMonstruos2.get(i));
-            }
-            for (int i = 0; i < listaMonstruos3.size(); i++) {
-                this.mapa = mapaAleatorio();
-                System.out.println("ESTAS EN: " + this.mapa);
-                siguesVivo = combate(listaMonstruos3.get(i));
-            }
-            for (int i = 0; i < listaMonstruos4.size(); i++) {
-                this.mapa = mapaAleatorio();
-                System.out.println("ESTAS EN: " + this.mapa);
-                siguesVivo = combate(listaMonstruos4.get(i));
-            }
-            for (int i = 0; i < listaMonstruos5.size(); i++) {
-                this.mapa = mapaAleatorio();
-                System.out.println("ESTAS EN: " + this.mapa);
-                siguesVivo = combate(listaMonstruos5.get(i));
-            }
-            //siguesvivo = llamar a combate(arraylist1.get())
-            // se carga array list 2
-            // se recorre el array list 2
-            //lamar a combate(arraylist2.get())
+        //while (siguesVivo) {
+        try {
+            lecturaMonstruos();
+        } catch (IOException ex) {
+            System.out.println("¡ERROR EN LA LECTURA DE MONSTRUOS (Fichero config.txt)!");
         }
+        for (int i = 0; i < listaMonstruos1.size() && siguesVivo; i++) {
+            this.mapa = mapaAleatorio();
+            System.out.println("[ZONA 1] ESTAS EN: " + this.mapa);
+            siguesVivo = combate(listaMonstruos1.get(i));
+            teclado.nextLine();
+        }
+        for (int i = 0; i < listaMonstruos2.size() && siguesVivo; i++) {
+            this.mapa = mapaAleatorio();
+            System.out.println("[ZONA 2] ESTAS EN: " + this.mapa);
+            siguesVivo = combate(listaMonstruos2.get(i));
+            teclado.nextLine();
+        }
+        for (int i = 0; i < listaMonstruos3.size() && siguesVivo; i++) {
+            this.mapa = mapaAleatorio();
+            System.out.println("[ZONA 3] ESTAS EN: " + this.mapa);
+            siguesVivo = combate(listaMonstruos3.get(i));
+            teclado.nextLine();
+        }
+        for (int i = 0; i < listaMonstruos4.size() && siguesVivo; i++) {
+            this.mapa = mapaAleatorio();
+            System.out.println("[ZONA 4] ESTAS EN: " + this.mapa);
+            siguesVivo = combate(listaMonstruos4.get(i));
+            teclado.nextLine();
+        }
+        for (int i = 0; i < listaMonstruos5.size() && siguesVivo; i++) {
+            this.mapa = mapaAleatorio();
+            System.out.println("[ZONA 5] ESTAS EN: " + this.mapa);
+            siguesVivo = combate(listaMonstruos5.get(i));
+            teclado.nextLine();
+        }
+        //siguesvivo = llamar a combate(arraylist1.get())
+        // se carga array list 2
+        // se recorre el array list 2
+        //lamar a combate(arraylist2.get())
     }
+    //}
 
     /**
      * Devuelve un String aleatorio del array mapas.
      */
     private String mapaAleatorio() {
-        return MAPAS_DISPONIBLES[(int) Math.round(Math.random() * (MAPAS_DISPONIBLES.length - 1))];
+        return MAPAS_DISPONIBLES[(int) Math.round(
+                Math.random() * (MAPAS_DISPONIBLES.length - 1))];
     }
-    
+
     public static void main(String[] args) {
-        Partida p1 = new Partida();
-        p1.jugar();
+        Scanner teclado = new Scanner(System.in);
+        String candado = "";
+        do {
+            Partida p1 = new Partida();
+            p1.jugar();
+            System.out.print("¿Quieres volver a jugar? [S / N]");
+            candado = teclado.nextLine();
+        } while (candado.toLowerCase().equals("s"));
     }
 }
